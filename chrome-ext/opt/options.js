b28af3b7c6e60fa218e1c64b776ee3bfcbba033f
TimR <tcrjk0+BB@gmail.com>
2017-04-25 23:18:54 -0600

    setup enhancements
    remove window.location.reload; statusbars; fix validation routines when
    adding a row; code cleanup

--- chrome-ext/opt/options.js
+++ chrome-ext/opt/options.js
@@ -5,59 +5,65 @@ $(document).ready(function() {
   loadLibraries();
 
   $('#btnSave').click(function() {
-    saveLibraries();
+    if (!quickAccess()) { saveLibraries(); }
   });
 
   $('#btnAdd').click(function() {
-    //TODO: quick access some MLS libraries for debugging
-    if ($('#shortName0').val() == "goLib1") {initLibraries(1);}
-    if ($('#shortName0').val() == "goLib2") {initLibraries(2);}
-    if ($('#shortName0').val() == "goLib3") {initLibraries(3);}
-    if ($('#shortName0').val() == "clearLib") {chrome.storage.sync.clear(); window.location.reload();}
-    addRow();
+    if (!quickAccess()) { $('#menuStatusBar').text("").hide(); addRow(); }
   });
 
   $('#btnSetup').click(function() {
-    //$('#btnSetup').text('working');
+    //TODO disable button on click, re-enable when done
+    $('btnSetup').prop('disabled', true);
+    $('#setupStatusBar').text("").hide();
+    $('#menuStatusBar').text("").hide();
     findLibraries();
   });
 
   $('#btnClearAndSetup').click(function() {
-    //Warn of serious nature and get confirmation to proceed - TODO s/b modal, confirm won't work
+    //TODO disable button on click, re-enable when done
+    $('btnClearAndSetup').prop('disabled', true);
+    // Warn of serious nature and get confirmation to proceed
+    // TODO: s/b modal, confirm won't work in chrome
     // r = window.confirm("Warning! This will delete all current libraries and replace them with those found in another tab.\n\nYou must be logged in to OverDrive.com with saved libraries before executing this command.");
     // if (r == true)
+    $('#setupStatusBar').text("").hide();
+    $('#menuStatusBar').text("").hide();
     findLibraries();
   });
 
   $('#btnCancelSetup').click(function() {
-    newLibraryIndex = 10000;
+    newLibraryIndex = 10000; //set number of Libraries high in case still running
+    //remove all listeners
+    chrome.webRequest.onCompleted.removeListener(foundSavedLibraries);
     chrome.webRequest.onCompleted.removeListener(runWhenLoaded);
-    $('#spinnerP').text('Cancelling...');
-    $('#spinner').fadeOut(1000);
-    setTimeout(function() {
-      window.location.reload();
-    },1000);
+    $('#spinnerP').css('color','red').text('Cancelling...');
+    $('#spinner').fadeOut(2000);
+    $('#setupStatusBar').text("Warning! Last setup canceled.").show();
+    $('btnSetup').prop('disabled', false); $('btnClearAndSetup').prop('disabled', false);
+    $('#menu').html(''); newLibraryIndex = 0; onNoLibraries = 5; loadLibraries();
   });
 });
 
 function findLibraries() {
   //clear former libraries
   chrome.storage.sync.clear();
-  console.log("requesting saved libraries")
-  //request saved libraries
-  $('#spinnerP').text('Downloading saved libraries ... please wait');
+  chrome.runtime.onMessage.addListener(foundSavedLibraries);
+  $('#spinnerP').css('color','black').text('Downloading saved libraries ... please wait');
   $('#spinner').fadeIn(1000);
+  //request saved libraries
+  console.log("requesting saved libraries")
   chrome.runtime.sendMessage({type: "_findSavedLibraries"});
 }
 
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+function foundSavedLibraries(message) {
   if (message.type == "_foundSavedLibraries") {
     $('#spinnerP').text('Found saved libraries ... working');
     console.log("from overdrive_mls _foundSavedLibraries",message);
     switch (message.status) {
       case "Success":
         libraries = message.response.newLibraries;
-        //save regardless
+        //save always, even before resolving vanity library URLs
         chrome.storage.sync.set({"libraries": message.response.newLibraries});
         newLibraryIndex = 0;
         var foundInvalid = false;
@@ -84,29 +90,36 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
             newLibraryIndex++;
           }
         }
-        //if all were well formed--save and clear spinner; otherwise, just return and wait for the listener to complete...
+        //if all were well formed--save and clear spinner; otherwise, just fall through to end of listener...
         if (!foundInvalid) {
           setupDone();
         }
         break;
       case "Not logged in":
-        $('#spinnerP').text(message.status);
+        $('#spinnerP').css('color','red').text(message.status);
         $('#spinner').fadeOut(2000);
         alert("Not logged into OverDrive. Please log into your OverDrive account to find library information from your saved libraries.");
-        setTimeout(function() {window.location.reload();},2000);
+        chrome.webRequest.onCompleted.removeListener(foundSavedLibraries);
+        $('#setupStatusBar').text("Warning! Last setup failed: No user is logged in on OverDrive.com").show();
+        $('btnSetup').prop('disabled', false); $('btnClearAndSetup').prop('disabled', false);
+        $('#menu').html(''); newLibraryIndex = 0; onNoLibraries = 5; loadLibraries();
         break;
       case "No saved libraries":
-        $('#spinnerP').text(message.status);
+        $('#spinnerP').css('color','red').text(message.status);
         $('#spinner').fadeOut(2000);
         alert("This OverDrive account has no saved libraries. Please save a list of local libraries to find library information for your local libraries (recommended), or enter them manually below.");
-        setTimeout(function() {window.location.reload();},2000);
+        chrome.webRequest.onCompleted.removeListener(foundSavedLibraries);
+        $('#setupStatusBar').text("Warning! Last setup failed: User has no saved libraries on OverDrive.com").show();
+        $('btnSetup').prop('disabled', false); $('btnClearAndSetup').prop('disabled', false);
+        $('#menu').html(''); newLibraryIndex = 0; onNoLibraries = 5; loadLibraries();
         break;
     }
-    console.log("end _foundSavedLibraries");
+    console.log("foundSavedLibraries removed");
+    chrome.webRequest.onCompleted.removeListener(foundSavedLibraries);
   }
-});
+}
 
-//listener to run when iframe loaded
+//listener to run when iframe loaded -- loops through all remaining libraries to determine URLs if needed
 function runWhenLoaded(iframeInfo) {
   console.log('runWhenLoaded: iframe loaded (onCompleted event fired), url: ', iframeInfo.url); //iframeSrcChanged
   //if resolved, change libraryURL to url of iframe url
@@ -144,16 +157,16 @@ function runWhenLoaded(iframeInfo) {
 
 function setupDone(){
   //done, need to sync or at least clear menu
-  $('#spinnerP').text('Setup Complete!');
-  $('#spinnerP').css('color','green');
+  $('#spinnerP').css('color','green').text('Setup Complete!');
   $('#spinner').fadeOut(2000);
+  $('btnSetup').prop('disabled', false); $('btnClearAndSetup').prop('disabled', false);
   console.log("setup done", libraries);
   $('#menu').html('');
   chrome.storage.sync.set({"libraries": libraries}, function() { onNoLibraries = 4; loadLibraries(); });
 };
 
 function loadLibraries() {
-	//try to retrieve library list from chrome.storage.sync; if not there, parse saved libraries list
+	//try to retrieve library list from chrome.storage.sync; if not there, parse saved debug libraries list
 	chrome.storage.sync.get("libraries", function(obj) {
 		libraries = obj["libraries"];
     console.log("libraries:", libraries);
@@ -186,7 +199,7 @@ function loadLibraries() {
       }
     }
     //console.log("libRows: " + libRows);
-    $('#menu').append(libRows);
+    $('#menu').html(libRows);
     $(".ip").off('change').on('change', validateInput);
   });
 }
@@ -212,20 +225,33 @@ function saveLibraries() {
       });
     }
   }
-  chrome.storage.sync.set({"libraries": librariesEdited});
-  window.location.reload();
+  chrome.storage.sync.set({"libraries": librariesEdited}, function(){
+    if (librariesEdited.length > 0) {
+      $('#menuStatusBar').css("color","green").text("Libraries saved").show();
+      $('#setup').children("*").addClass("hidden");
+      $('#normal').children("*").removeClass("hidden");
+      $('#menu').html(''); libraries = librariesEdited; newLibraryIndex = 0; onNoLibraries = 5; loadLibraries();
+    } else {
+      $('#setupStatusBar').text("Libraries manually cleared").show();
+      $('#setup').children("*").removeClass("hidden");
+      $('#normal').children("*").addClass("hidden");
+      $('#menuStatusBar').css("color","red").text("Libraries manually cleared").show();
+      $('#menu').html(''); libraries = []; newLibraryIndex = 0; onNoLibraries = 5; loadLibraries();
+    }
+  });
 }
 
 function initLibraries(onNoLibraries) {
-  //single library onNoLibraries
+  //TODO: remove the quick access some MLS libraries for debugging
   //console.log('onNoLibraries: ' + onNoLibraries);
   if (onNoLibraries == 1) {
     libraries = [{ "libraryFullName": "Douglas County Libraries", "libraryShortName": "Douglas", "libraryURL": "dcl.overdrive.com" }];
-    //save libraries
-    chrome.storage.sync.set({"libraries": libraries}, function() { });
-    window.location.reload();
+    chrome.storage.sync.set({"libraries": libraries}, function() {
+      $('#setupStatusBar').text("").hide();
+      $('#menuStatusBar').text("").hide();
+      loadLibraries();
+    });
   }
-  //full set
   if (onNoLibraries == 2) {
     libraries = [
     	{ "libraryShortName": "Arapahoe", "libraryURL": "Arapahoe.overdrive.com", "libraryFullName": "Arapahoe Library District" },
@@ -236,21 +262,26 @@ function initLibraries(onNoLibraries) {
     	{ "libraryShortName": "Anythink", "libraryURL": "AnythinkLibraries.overdrive.com", "libraryFullName": "Rangeview Library District" },
     	{ "libraryShortName": "Douglas", "libraryURL": "dcl.overdrive.com", "libraryFullName": "Douglas County Libraries" }
     ];
-    //save libraries
-    chrome.storage.sync.set({"libraries": libraries}, function() { });
-    window.location.reload();
+    chrome.storage.sync.set({"libraries": libraries}, function() {
+      $('#setupStatusBar').text("").hide();
+      $('#menuStatusBar').text("").hide();
+      loadLibraries();
+    });
   }
   if (onNoLibraries == 3) {
     libraries = [
-      { "libraryShortName": "Boulder", "libraryURL": "FrontRange.overdrive.com", "libraryFullName": "Front Range Downloadable Library" },
-      { "libraryShortName": "Poudre", "libraryURL": "PoudreRiver.libraryreserve.com", "libraryFullName": "Poudre River Library District" },
+      { "libraryShortName": "Denver", "libraryURL": "Denver.overdrive.com", "libraryFullName": "Denver Public Library" },
       { "libraryShortName": "Arapahoe", "libraryURL": "Arapahoe.overdrive.com", "libraryFullName": "Arapahoe Library District" },
-      { "libraryShortName": "Denver", "libraryURL": "Denver.overdrive.com", "libraryFullName": "Denver Public Library" }
+      { "libraryShortName": "Poudre", "libraryURL": "PoudreRiver.libraryreserve.com", "libraryFullName": "Poudre River Library District" },
+      { "libraryShortName": "Boulder", "libraryURL": "FrontRange.overdrive.com", "libraryFullName": "Front Range Downloadable Library" }
     ];
-    //save libraries
-    chrome.storage.sync.set({"libraries": libraries}, function() { });
-    window.location.reload();
+    chrome.storage.sync.set({"libraries": libraries}, function() {
+      $('#setupStatusBar').text("").hide();
+      $('#menuStatusBar').text("").hide();
+      loadLibraries();
+    });
   }
+  //trigger on run: may eventually be used to auto setup?
   if (onNoLibraries == 4) {
     onNoLibraries = 0;
     console.log('b4findLib');
@@ -298,9 +329,9 @@ function addRow() {
   var index = 0;
   if (libraries) { index = libraries.length; }
   var libRows = "<tr>";
-  libRows += "<td><input id='shortName" + index + "' value='' class= 'ip ip1' maxlength='15' type='text'/></td>";
-  libRows += "<td><input id='URL" + index + "' value='' class= 'ip' type='text'/></td>";
-  libRows += "<td><input id='fullName" + index + "' value='' class= 'ip' type='text'/></td>";
+  libRows += "<td><input id='shortName" + index + "' value='' class='ip ip1' maxlength='15' type='text'/></td>";
+  libRows += "<td><input id='URL" + index + "' value='' class='ip badURL' type='text'/></td>";
+  libRows += "<td><input id='fullName" + index + "' value='' class='ip' type='text'/></td>";
   libRows += "</tr>";
   if (libraries) { libraries.push({ "libraryFullName": "", "libraryShortName": "", "libraryURL": "" }); }
   else { libraries = [{ "libraryFullName": "", "libraryShortName": "", "libraryURL": "" }]; }
@@ -314,25 +345,30 @@ String.prototype.firstWord = function (){
 		return (this.indexOf(' ') !== -1) ? this.substr(0, this.indexOf(' ')) : this;
 };
 
-function validateInput() {
-  //console.dir(this);
-  if ($(this).length == 0) {return;}
+function validateInput(elem) {
+  //console.dir(elem);
+  if (typeof $(elem).attr('id') === "undefined") {elem = elem.target; console.log("elem set to elem.target",elem);} else {console.log("elem ok", elem)}
+  if ($(elem).length == 0) {return;}
   //if an URL
-  if ($(this).attr('id').indexOf("RL") > 0) {
-    $(this).val( $(this).val().replace(/[^\.a-z0-9:\/ _]/gi,'') ); //limit allowable characters
-    $(this).val($(this).val().trim()); //trim it up
-    $(this).val($(this).val().replace(/^https?:\/\//, '').replace(/\.overdrive\.com.*/i,'.overdrive.com'.replace(/\.libraryreserve\.com.*/i,'.libraryreserve.com').trim()));
-    var thisurl = $(this).val();  //this is the URL as it would be saved
+  if ($(elem).attr('id').indexOf("RL") > 0) {
+    $(elem).val( $(elem).val().replace(/[^\.a-z0-9:\/ _]/gi,'') ); //limit allowable characters
+    $(elem).val($(elem).val().trim()); //trim it up
+    $(elem).val($(elem).val().replace(/^https?:\/\//, '').replace(/\.libraryreserve\.com.*/i,'.libraryreserve.com').replace(/\.overdrive\.com.*/i,'.overdrive.com'.trim()));
+    var thisurl = $(elem).val();  //thisurl is the URL
     if (thisurl.length > 0) {
-      console.log(thisurl);
-      if (isValidURL(thisurl))
-      { $(this).removeClass('badURL');
-      } else { $(this).addClass('badURL'); }
+      console.log("thisurl: " + thisurl);
+      if (isValidURL(thisurl)) {
+        $(elem).removeClass('badURL');
+      } else {
+        $(elem).addClass('badURL');
+      }
+    } else {
+      $(elem).addClass('badURL');
     }
   } else {
     //if not an URL
-    $(this).val( $(this).val().replace(/[^a-z0-9 _]/gi,'') ); //limit allowable characters
-    $(this).val($(this).val().trim());
+    $(elem).val( $(elem).val().replace(/[^a-z0-9 _]/gi,'') ); //limit allowable characters
+    $(elem).val($(elem).val().trim());
   }
 }
 
@@ -345,4 +381,20 @@ function isValidURL(thisurl) {
       && thisurl.indexOf("www.libraryreserve.com") == -1 )
     ) {return true;}
   else {return false;}
+}
+
+function quickAccess(){
+  //TODO: remove quick access some MLS libraries for debugging
+  if ($('#shortName0').val() == "goLib1") {initLibraries(1); return true;}
+  if ($('#shortName0').val() == "goLib2") {initLibraries(2); return true;}
+  if ($('#shortName0').val() == "goLib3") {initLibraries(3); return true;}
+  if ($('#shortName0').val() == "clearLib") {
+    chrome.storage.sync.clear(function() {
+      $('#setupStatusBar').text("Cleared all libraries").show();
+      $('#menuStatusBar').text("Cleared all libraries").show();
+      $('#menu').html(''); libraries = []; newLibraryIndex = 0; onNoLibraries = 5; loadLibraries();
+    });
+    return true;
+  }
+  return false;
 }
